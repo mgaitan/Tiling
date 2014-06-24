d56e8d8e6f6fea248fc35c4e56afa0b5b07bc7e2
Martín Gaitán <gaitan@phasety.com>
2014-06-24 17:03:21 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import re
+from os import path
+import codecs
+from distutils.core import setup
+
+init_path = path.join(path.dirname(__file__), 'tiling', '__init__.py')
+with codecs.open(init_path, 'r', 'utf-8') as fd:
+    VERSION = re.search("__version__ = '([^']+)'", fd.read().strip()).group(1)
+
+setup(name='Tiling',
+      version=VERSION,
+      description='Tilings of regular polygons',
+      long_description=codecs.open('README.rst', 'r', 'utf-8').read(),
+      author='Michael Fogleman',
+      author_email='michael.fogleman@gmail.com',
+      url='https://github.com/fogleman/Tiling',
+      install_requires=['pycairo'],
+      packages=['tiling'],
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: "
+        "GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Multimedia :: Graphics"]
+     )
